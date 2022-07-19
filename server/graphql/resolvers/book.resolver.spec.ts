626cccab474204b1b82e98332813c92c1b31906c
Victoria Cabezos <victoria.cabezos@origins-digital.com>
2022-07-19 12:23:36 +0200

    resolver add book

---
+++ server/graphql/resolvers/book.resolver.spec.ts
@@ -0,0 +1,11 @@
+export interface IAddBookInput {
+  addBookInput: {
+    title: string;
+    author: string;
+  };
+}
+
+export interface IBook {
+  title: string;
+  author: string;
+}
--- server/graphql/resolvers/books.resolver.ts
+++ server/graphql/resolvers/books.resolver.ts
@@ -1,4 +1,7 @@
-let books = [
+import { GraphQLObjectType } from "graphql";
+import { IAddBookInput, IBook } from "./book.resolver.spec";
+
+let books: Array<IBook> = [
   {
     title: "The Awakening",
     author: "Kate Chopin",
@@ -12,5 +15,12 @@ let books = [
 export default {
   Query: {
     listBooks: () => books,
+  },
+  Mutation: {
+    addBook: (_: GraphQLObjectType, { addBookInput }: IAddBookInput) => {
+      let newBook = { ...addBookInput };
+      books = [...books, newBook];
+      return newBook;
+    },
   },
 };
